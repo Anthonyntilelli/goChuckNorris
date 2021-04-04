2d2b048ea80a70dd5d0f1a68cdfb8493733cf511
Anthony Tilelli <Anthony@tilelli.me>
2021-04-03 22:34:50 -0400

    random Joke

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+
+	responce, _ := getRandomFact(FactURL)
+	fmt.Println(string(responce))
+}
+
+const FactURL string = "https://api.chucknorris.io/"
+
+func getRandomFact(url string) ([]byte, error) {
+	resp, err := http.Get(url + "/jokes/random")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
