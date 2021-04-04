8b9ea867ee074fe79c291b5ba60772497735d905
Anthony Tilelli <Anthony@tilelli.me>
2021-04-04 15:49:02 -0400

    Move get to own function

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
 func main() {
 
-	responce, err := getJokesbytext("potato")
+	responce, err := getRandomFact()
 	if err != nil {
 		fmt.Println("ERROR")
 		fmt.Println(err)
@@ -21,42 +22,15 @@ func main() {
 const FactURL string = "https://api.chucknorris.io/"
 
 func getRandomFact() ([]byte, error) {
-	resp, err := http.Get(FactURL + "/jokes/random")
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	return body, nil
+	return getAPI(FactURL + "/jokes/random")
 }
 
 func getCategorieslist() ([]byte, error) {
-	resp, err := http.Get(FactURL + "jokes/categories")
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	return body, nil
+	return getAPI(FactURL + "jokes/categories")
 }
 
 func getRandomFactByCategory(category string) ([]byte, error) {
-	resp, err := http.Get(FactURL + "/jokes/random?category=" + category)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	return body, nil
+	return getAPI(FactURL + "/jokes/random?category=" + category)
 }
 
 func getJokesbytext(text string) ([]byte, error) {
@@ -64,9 +38,16 @@ func getJokesbytext(text string) ([]byte, error) {
 		return nil, fmt.Errorf("text must be one word")
 	}
 
-	resp, err := http.Get(FactURL + "jokes/search?query=" + text)
+	return getAPI(FactURL + "jokes/search?query=" + text)
+}
+
+func getAPI(fullUrl string) ([]byte, error) {
+	resp, err := http.Get(fullUrl)
 	if err != nil {
 		return nil, err
+	}
+	if (err == nil) && (resp.StatusCode != 200) {
+		return nil, fmt.Errorf("Return status code is" + strconv.Itoa(resp.StatusCode))
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
