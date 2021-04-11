d006f86a2a7da0c232a90c1427522441076e7a37
Anthony Tilelli <Anthony@tilelli.me>
2021-04-10 20:29:27 -0400

    add base command line flags

--- main.go
+++ main.go
@@ -1,17 +1,60 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-
-	"github.com/Anthonyntilelli/goChuckNorris/chuckApi"
+	"os"
 )
 
+// "github.com/Anthonyntilelli/goChuckNorris/chuckApi"
+
 func main() {
-	fact, err := chuckApi.RandomFact()
-	if err != nil {
-		fmt.Println("ERROR")
-		fmt.Println(err)
-	} else {
-		fmt.Println(fact)
+	// fact, err := chuckApi.RandomFact()
+	// if err != nil {
+	// 	fmt.Println("ERROR")
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println(fact)
+	// }
+	var searchTerm, category string
+	var showCategories, random bool
+	set := 0
+
+	flag.StringVar(&searchTerm, "s", "", "Get a fact based on search Term [choose only one flag]")
+	if searchTerm != "" {
+		set++
+		fmt.Println("set ", set) //DIAG
+	}
+	flag.StringVar(&category, "k", "", "Get a fact based on category [choose only one flag]")
+	if category != "" {
+		set++
+		fmt.Println("set ", set) //DIAG
+	}
+	flag.BoolVar(&showCategories, "c", false, "Shows available categories [choose only one flag]")
+	if showCategories {
+		set++
+		fmt.Println("set ", set) //DIAG
+	}
+	flag.BoolVar(&random, "r", false, "Shows a random fact [choose only one flag]")
+	if random {
+		set++
+		fmt.Println("set ", set) //DIAG
+	}
+	flag.Parse()
+
+	//DIAG
+	fmt.Print("searchTerm ", searchTerm, "\n")
+	fmt.Print("category ", category, "\n")
+	fmt.Print("ShowCategories ", showCategories, "\n")
+	fmt.Print("random ", random, "\n")
+	fmt.Print("set ", set, "\n")
+	//DIAG
+
+	if set == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+	if set != 1 {
+		fmt.Fprintf(os.Stderr, "Only Select 1 flag\n")
 	}
 }
